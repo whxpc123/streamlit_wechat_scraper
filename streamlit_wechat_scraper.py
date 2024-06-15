646d264dc39f037f98316a4c1a2f4188b632c851
whxpc123 <45917083+whxpc123@users.noreply.github.com>
2024-06-16 01:01:25 +0800

    Update streamlit_wechat_scraper.py

--- streamlit_wechat_scraper.py
+++ streamlit_wechat_scraper.py
@@ -25,7 +25,7 @@ if start_button:
     options.add_argument('--disable-gpu')  # 禁用GPU加速
 
     # 手动指定 ChromeDriver 的路径
-    chrome_driver_path = os.path.join(os.getcwd(), 'chromedriver')
+    chrome_driver_path = 'chromedriver.exe'
     driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
 
     # 目标网址
